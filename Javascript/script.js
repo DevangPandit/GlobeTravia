b75fb3e4354582081cb472c159891a1d3d353999
DevangPandit <panditdevang7@gmail.com>
2025-02-17 18:16:15 -0500

    Update in login and create pages

--- Javascript/script.js
+++ Javascript/script.js
@@ -24,3 +24,45 @@ scrollReveal.reveal(".reviews_card", {
     ...ScrollRevealOption,
     interval: 500,
 });
+
+function validatefunc(){
+    let x = document.forms["myForm"]["femail"].value;
+    let errorMessage = document.getElementById("error-message");
+    errorMessage.innerHTML="";
+
+    if (x === ""){
+        errorMessage.innerHTML = "This field must be filled.";
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function validateform(){
+    let x = document.forms["myForm"]["fpass"].value;
+    let y = document.forms["myForm"]["frepass"].value;
+    let errorMessage1 = document.getElementById("error-message1");
+
+    errorMessage1.innerHTML = "";
+    
+    if (x !== y ){
+        errorMessage1.innerHTML = "password does not matches."
+        return false;
+    }
+    else if(x == ""){
+        errorMessage1.innerHTML = "password does not matches."
+        return false;
+    }
+    else{
+        return true;
+    }
+
+}
+
+function validateAll(){
+    let isEmailValid = validatefunc();
+    let isPasswordValid = validateform();
+
+    return isEmailValid && isPasswordValid;
+}
